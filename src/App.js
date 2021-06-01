9d7a865972f6469d990745d5d96c11b2ea0627e0
LoganBlack <loganblack88@gmail.com>
2021-06-01 18:22:11 -0500

    Add basic routing & bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import  Home  from './Home/Home';
+import './App.scss';
+import {Navbar} from "react-bootstrap";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Team Hangman</Navbar.Brand>
+      </Navbar>
+
+      <main>
+        <HashRouter>
+          <div>
+            <Switch>
+              <Route path={`/game/:gameId`}>
+                <h2>GAME</h2>
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </HashRouter>
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+import './Home.scss';
+
+
+export default function Home() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Home</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
